fdfa4eafee4813c1eddb87c8e50a5a281489ae40
vendi <svitlana.vakulenko@gmail.com>
2019-12-09 20:57:13 +0100

    chg: debug

--- web_app/app.py
+++ web_app/app.py
@@ -16,4 +16,4 @@ app = Flask(__name__)
 app.register_blueprint(search_blueprint)
 
 if __name__ == "__main__":
-    app.run("0.0.0.0",port=8008,debug=False,threaded=True)
+    app.run("0.0.0.0", port=8008, debug=True, threaded=True)
