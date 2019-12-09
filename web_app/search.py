d5704d6b9e793f3fdf77ae544f8e0db3088df327
vendi <svitlana.vakulenko@gmail.com>
2019-12-09 21:12:48 +0100

    chg: debug

--- web_app/search.py
+++ web_app/search.py
@@ -1,11 +1,11 @@
-from flask import Blueprint,render_template,request,jsonify
+from flask import Blueprint, render_template, request, jsonify
 import requests,json
 
 from settings import *
 
 
 # creating a Blueprint class
-search_blueprint = Blueprint('search',__name__,template_folder="templates")
+search_blueprint = Blueprint('search', __name__, template_folder="templates")
 search_term = ""
 
 
@@ -14,13 +14,13 @@ headers = {
     'cache-control': "no-cache",
 }
 
-@search_blueprint.route("/",methods=['GET','POST'],endpoint='index')
+@search_blueprint.route("/", methods=['GET','POST'], endpoint='index')
 def index():
     if request.method=='GET':
         res ={
                 'hits': {'total': 0, 'hits': []}
              }
-        return render_template("index.html",res=res)
+        return render_template("index.html", res=res)
     elif request.method =='POST':
         if request.method == 'POST':
             print("-----------------Calling search Result----------")
@@ -43,4 +43,5 @@ def index():
             url = "http://localhost:9200/%s/%s/_search" % (INDEX_NAME, TYPE_NAME)
             response = requests.request("GET", url, data=payload, headers=headers)
             response_dict_data = json.loads(str(response.text))
+            print(response_dict_data)
             return render_template('index.html', res=response_dict_data)
