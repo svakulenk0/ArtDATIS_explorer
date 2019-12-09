65adc41a26b7391227b76b5866814ce6500b4309
vendi <svitlana.vakulenko@gmail.com>
2019-12-09 17:13:28 +0100

    fix: indexing

--- index.py
+++ index.py
@@ -14,27 +14,28 @@ import glob
 
 txts_path = '%s/artdatis/tagging/OCRed/typed/' % DATA_PATH
 text_corpus = []
-bulk_data = [] 
-# filter out and collect text files
-for file_path in glob.glob(txts_path+'*_text.txt'):
-    with open(file_path, encoding="utf-8") as file:
-        text = file.read()
-        # filter duplicates
-        if text not in text_corpus:
-            text_corpus.append(text)
-            with open(file_path.split('_text.txt')[0]+'_path.txt') as path_file:
-                path = path_file.read().strip()
-                # create the _source data for the Elasticsearch doc
-                data_dict = {
-                    "file_path": path,
-                    "text": text
-                }
-               
-                bulk_data.append({"_index": INDEX_NAME, "_type": TYPE_NAME, "_source": data_dict})
-print("Loaded %d documents"%len(text_corpus))
+
+def corpus_iterator():
+    # filter out and collect text files
+    for file_path in glob.glob(txts_path+'*_text.txt'):
+        with open(file_path, encoding="utf-8") as file:
+            text = file.read()
+            # filter duplicates
+            if text not in text_corpus:
+                text_corpus.append(text)
+                with open(file_path.split('_text.txt')[0]+'_path.txt') as path_file:
+                    path = path_file.read().strip()
+                    yield {
+                            "_index": INDEX_NAME,
+                            "_type": TYPE_NAME,
+                            "doc": {"file_path": path, "text": text},
+                        }
+    print("Loaded %d documents"%len(text_corpus))
 
 
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
 # create ES client, create index
 es = Elasticsearch(hosts = [ES_HOST])
 if es.indices.exists(INDEX_NAME):
@@ -54,7 +55,7 @@ print(" response: '%s'" % (res))
 
 # bulk index the data
 print("bulk indexing...")
-res = es.bulk(index = INDEX_NAME, body = bulk_data, refresh = True)
+bulk(es, corpus_iterator())
 
 # sanity check
 res = es.search(index = INDEX_NAME, size=2, body={"query": {"match_all": {}}})
